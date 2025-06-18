6ab88148e86a9ffe1ec27369d76d9a7ed666b940
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-18 11:13:47 +0200

    exo8

--- exo/exo8.php
+++ exo/exo8.php
@@ -1,3 +1,13 @@
 <?php
+$a = 14;
+$b = 22;
+$c = 6;
 
+if ($a > $b && $a > $c ){
+    echo 'le plus grand nombre est ' . $a;
+}elseif ($b > $a && $b > $c){
+    echo 'le plus grand nombre est ' . $b;
+}else {
+    echo 'le plus grand nombre est ' . $c;
+}
 ?>
